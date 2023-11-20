fc39b0114288caf13ce6ac0252faff5bf7176583
Arthur <88573504+TheArtur128@users.noreply.github.com>
2023-11-21 00:03:25 +0700

    Add `2.1`

---
+++ lessons/2/1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+
+int _boxCount = 15;
+int &refBoxCount = _boxCount;
+
+
+int _carNumber = 0;
+void WriteNumberOfLoadedBoxesTo(int &refNumberOfLoadedBoxes)
+{
+    _carNumber++;
+
+    cout << "Всего на складе " << refBoxCount << " коробок" << endl;
+    cout << "Сколько загрузить ящиков в машину N" << _carNumber << "?\n>> ";
+    
+    cin >> refNumberOfLoadedBoxes;
+}
+
+
+void OnOverflow(int &_)
+{
+    cout << "На складе только " << refBoxCount << " коробок" << endl;
+}
+
+
+void Do(int &refBoxCount)
+{
+    int *pNumberOfLoadedBoxes;
+    
+    cout << refBoxCount;
+
+    do {
+        WriteNumberOfLoadedBoxesTo(*pNumberOfLoadedBoxes);
+
+        if (*pNumberOfLoadedBoxes > refBoxCount) {
+            OnOverflow(*pNumberOfLoadedBoxes);
+            continue;
+        }
+
+        refBoxCount -= *pNumberOfLoadedBoxes;
+    } while (refBoxCount > 0);
+}
+
+
+void OnExit()
+{
+    cout << "На складе больше нет коробок" << endl;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Do(refBoxCount);
+    OnExit();
+}
